2fd169ec89711e0ee01f2787083dcd179a5337c4
protivaghosh <protivaghosh50@gmail.com>
2025-08-24 14:40:24 +0600

    make add to add money section

--- home.js
+++ home.js
@@ -1,9 +1,20 @@
 document.getElementById("add-money-button").addEventListener("click", function(e){
 e.preventDefault()
-console.log("added to add money lissener")
+//console.log("added to add money lissener")
 const bank = document.getElementById("bank").value
 const amountNumber = document.getElementById("acount-number").value
 const addAmount = parseInt(document.getElementById("add-amount").value)
 const pinNumber = document.getElementById("pin-number").value
-console.log(bank, amountNumber, addAmount, pinNumber);
+
+const availableBalance = parseInt(document.getElementById("available-balance").innerText)
+
+//console.log(availableBalance)
+if(amountNumber.length < 11){
+    alert("please provide valid account number");
+    return;
+}
+
+const newAvailableBalance = addAmount + availableBalance;
+
+document.getElementById("available-balance").innerText=newAvailableBalance
 })
